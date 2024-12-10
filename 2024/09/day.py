3e1bd4402ab7a0f0837770115b2348b7312e0970
Richard Bronkhorst <richmans@users.noreply.github.com>
2024-12-10 20:24:59 +0000

    Day 9 and 10

--- 2024/09/day.py
+++ 2024/09/day.py
@@ -1,8 +1,56 @@
 def parse_data(raw):
-  return raw
+  parts = []
+  is_file = True
+  pos = 0
+  fid = 0
+  for n in raw:
+    n = int(n)
+    if is_file:
+      parts.append([fid, pos, n])
+      fid += 1
+    else:
+      parts.append([None, pos, n])
+    pos += n
+    is_file = not is_file
+  return parts
 
 def solve_part1(data):
-  return 0
+  rev = [d for d in data[::-1] if d[0] is not None]
+  res = 0
+  for b in data:
+    l = b[2]
+    p = b[1]
+    fid = b[0]
+    if fid is not None:
+      res += int(fid * l * (p + (l-1) / 2))
+      b[0] = 0
+      continue
+    while l > 0:
+      n = min(l, rev[0][2])
+      fid = rev[0][0]
+      l -= n
+      res += int(fid * n * (p + (n-1) / 2))
+      rev[0][2] -= n
+      if rev[0][2] == 0:
+        rev.pop(0)
+      p += n
+  return res
 
 def solve_part2(data):
-  return 0
+  frees = [d for d in data if d[0] is None]
+  files = [d for d in data if d[0] is not None][::-1]
+  res = 0
+  for fi in files:
+    fid = fi[0]
+    l = fi[2]
+    p = fi[1]
+    for fr in frees:
+      if fr[1] > p:
+        break
+      if fr[2] >= l:
+        fr[2] -= l
+        p = fr[1]
+        fr[1] += l
+        break
+    res += int(fid * l * (p + (l-1) / 2))
+  return res 
--- 2024/10/day.py
+++ 2024/10/day.py
@@ -1,8 +1,66 @@
 def parse_data(raw):
-  return raw
+  return raw.splitlines()
+
+def get_heads(data):
+  nodes = {}
+  w = len(data[0])
+  for y, line in enumerate(data):
+    for x, c in enumerate(line):
+      if c == '0':
+        nodes[(x, y)] = set([y*w+x])
+  return nodes
+
+def neigh(x,y,w,h):
+  n = set()
+  if x > 0:
+    n.add((x-1,y))
+  if x < w-1:
+    n.add((x+1,y))
+  if y > 0:
+    n.add((x,y-1))
+  if y < h-1:
+    n.add((x,y+1))
+  return n
+  
+def solve_part2(data):
+  nodes = get_heads(data)
+  nodes = {k:1 for k in nodes}
+  w = len(data[0])
+  h = len(data)
+  for ht in range(1,10):
+    new_nodes = {}
+    for n, heads in nodes.items():
+      for nx, ny in neigh(*n,w,h):
+        nc = int(data[ny][nx])
+        if nc == ht:
+          ns = new_nodes.setdefault((nx,ny), 0)
+          new_nodes[(nx,ny)] = ns + heads
+    nodes = new_nodes
+  return sum(nodes.values())
 
 def solve_part1(data):
-  return 0
+  nodes = get_heads(data)
+  w = len(data[0])
+  h = len(data)
+  for ht in range(1,10):
+    new_nodes = {}
+    for n, heads in nodes.items():
+      for nx, ny in neigh(*n,w,h):
+        nc = int(data[ny][nx])
+        if nc == ht:
+          ns = new_nodes.setdefault((nx,ny), set())
+          ns.update(heads)
+    nodes = new_nodes
+  return sum(len(heads) for heads in nodes.values())
 
-def solve_part2(data):
-  return 0
+
+example_a = example_b = """89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732"""
+
+example_answer_b = 81
